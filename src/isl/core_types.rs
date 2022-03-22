6f2b00e3390da81b0acbe978ee76ad1a34ccbaf7
Khushboo Desai <desaikd@amazon.com>
2022-03-22 15:48:57 -0700

    remove unnecessary file  from implementaion

--- src/isl/core_types.rs
+++
@@ -1,8 +0,0 @@
-HashMap::from([
-    ("any", "type::{ name: any, one_of: [ blob, bool, clob, decimal,
-                                    float, int, string, symbol, timestamp,
-                                    list, sexp, struct ] }"),
-    ("lob" , "type::{ name: lob, one_of: [ blob, clob ] }"),
-    ("number", "type::{ name: number, one_of: [ decimal, float, int ] }"),
-    ("text", "type::{ name: text, one_of: [ string, symbol ] }")
-])
